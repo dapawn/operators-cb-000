5609570e01ca746db5f031f41202fcd980e8de26
David Kuruvilla <dapawn@gmail.com>
2017-10-17 17:40:47 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -7,5 +7,12 @@ def unsafe?(speed)
 end
 
 def not_safe?(speed)
-	speed < 40 && speed > 60 ? true : false
+	speed < 40 && speed > 60 ? true : false;
 end
+
+puts unsafe?(35)
+puts unsafe?(70)
+puts unsafe?(55)
+puts not_safe?(35)
+puts not_safe?(70)
+puts not_safe?(55)
