0b7b4b698af4a201b04f820b1fddf6a3c1f28918
David Kuruvilla <dapawn@gmail.com>
2017-10-17 17:41:56 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -10,9 +10,3 @@ def not_safe?(speed)
 	speed < 40 || speed > 60 ? true : false;
 end
 
-puts unsafe?(35)
-puts unsafe?(70)
-puts unsafe?(55)
-puts not_safe?(35)
-puts not_safe?(70)
-puts not_safe?(55)
